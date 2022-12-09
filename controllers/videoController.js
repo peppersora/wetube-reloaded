4bddd67e5fb0176025fa57625bbf245ca56cc208
peppersora <110614387+peppersora@users.noreply.github.com>
2022-12-10 02:19:49 +0900

    search end

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -109,7 +109,9 @@ export const search = async (req,res) => {
   let videos =[];
   if(keyword){
    videos =await Video.find({
-    title: keyword,
+    title: {
+      $regex: new RegExp(`${keyword}`,"i"),
+    }
    });
   }
   return res.render("search",{pageTitle:"Search",videos});
