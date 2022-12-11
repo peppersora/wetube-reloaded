eddeed96b18c33cb2e9469a4fb5e09e1d2780992
peppersora <110614387+peppersora@users.noreply.github.com>
2022-12-11 16:52:17 +0900

    login(post)

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,20 +20,41 @@ export const postJoin = async (req,res) =>{
         return res.status(400).render("join",{
         pageTitle ,
         errorMessage: "This username/email is already taken.",
-        })
+        });
     }
-   
-    await User.create({
-        name,
-        username, 
-        email, 
-        password, 
-        location, 
+    try {
+        await User.create({
+            name,
+            username, 
+            email, 
+            password, 
+            location, 
+        });
+        return res.redirect("/login");
+    } catch(error){
+        return res.status(400).render("join", {
+            pageTitle: "Upload Video",
+            errorMessage: "error.message,"
+        })};//db에서 에러를 방지하기 위해 try-catch 사용
+};
+export const getLogin = (req, res) =>
+    res.render("login",{pageTitle: "Login"});
+
+
+export const postLogin = async (req,res) => {
+    const {username, password} = req.body;
+    const exists = await User.exists({username});
+    if(!exists){
+        return res.status(400).render("login",
+        { pageTitle: "Login",
+        errorMessage: "An account with this username does not exists.",
     });
-    return res.redirect("/login");
+    }
+    // check if account exists
+    // check if password correct
+    return res.end();
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
