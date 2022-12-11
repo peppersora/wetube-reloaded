b112d1a3d17f9d201faa2f44433de44390832876
peppersora <110614387+peppersora@users.noreply.github.com>
2022-12-11 22:38:11 +0900

    session & cookie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 import express, { urlencoded } from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -19,6 +20,19 @@ app.set("view engine","pug");
 app.set("views", process.cwd() + "/views");
 app.use(logger);
 app.use(express.urlencoded({extended:true}));
+app.use(
+    session({
+        secret: "Hello",
+        resave:true,
+        saveUninitialized: true,
+
+    })
+);
+
+app.get("/add-one",(req,res,next) => {
+    req.session.potato += 1;
+    return res.send(`${req.session.id}\n${req.session.potato}`);
+});
 app.use("/",rootRouter);
 app.use("/videos",videoRouter);
 app.use("/users",userRouter);
