2694902fbd996754410ff525cb005103f1481adc
peppersora <110614387+peppersora@users.noreply.github.com>
2022-12-14 15:06:38 +0900

    github email

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 import { application } from "express";
 export const getJoin = (req, res) => res.render("join",{pageTitle: "Join"});
@@ -90,14 +91,38 @@ export const finishGithubLogin = async (req,res) => {
     };
     const params = new URLSearchParams(config).toString();
     const finalUrl = `${baseUrl}?${params}`;
-    const data = await fetch(finalUrl,{
+    const tokenRequest = await (
+    await fetch(finalUrl,{
         method:"POST",
         headers: {
             Accept: "application/json",
-        }
+        },
     })
-    const json = await data.json();
-    
+    ).json();
+ 
+    if("access_token" in tokenRequest){
+        //access api
+        const {access_token} = tokenRequest;
+        const apiUrl = "https://api.github.com";
+        const userData = await (
+            await fetch(`${apiUrl}/user`,{
+            headers: {
+                Authorization: `token ${access_token}`,
+            },
+        })
+        ).json(); 
+        console.log(userData);
+        const emailData = await (
+            await fetch(`${apiUrl}/user/emails`,{
+            headers: {
+                Authorization: `token ${access_token}`,
+            },
+            })
+        ).json();
+        console.log(emailData);
+    }else{
+        return res.redirect("/login");
+    };
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
