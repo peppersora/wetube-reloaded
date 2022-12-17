58b576ef85c810f3f4c74cf755be42c68b6d5381
peppersora <110614387+peppersora@users.noreply.github.com>
2022-12-17 23:34:20 +0900

    Frontend setting

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// 굉장히 오래된 자스 코드만 이해할 수 있음
+//  주의할 점 두가지
+// 1. entry: 우리가 처리하고자하는 파일 =>main.js
+// webpack이 읽을 configuration 파일으 내보낼것임
+// console.log(__dirname); => 파일까지의 전체경로
+const path = require("path");
+// console.log(path.resolve(__dirname, "assets","js"));
+module.exports = {
+    entry: "./client/js/main.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "assets","js"),
+    },
+};
