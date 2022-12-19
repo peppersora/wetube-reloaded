64908f635ceb1807f6755d959f2e34bec3e23170
peppersora <110614387+peppersora@users.noreply.github.com>
2022-12-19 21:06:10 +0900

    video player

--- client/js/videoPlayer.js
+++ client/js/videoPlayer.js
@@ -1,9 +1,15 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currenTime = document.getElementById("currenTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreen = document.getElementById("fullScreen");
+const videoControls = document.getElementById("videoControls");
 
+let controlsTimeout = null;
+let controlsMovementTimeout = null;
 let volumeValue = 0.5;
 video.volume=volumeValue;
 
@@ -27,8 +33,6 @@ const handleMute =(e) =>{
     volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
-playBtn.addEventListener("click",handlePlayClick);
-muteBtn.addEventListener("click",handleMute);
 
 const handleVolumeChange = (event) =>{
     const {
@@ -39,8 +43,71 @@ const handleVolumeChange = (event) =>{
         video.innerText ="Mute";
     }
     volumeValue = value;
-        video.volume=value;
+    video.volume=value;
     
 };
 
+const formatTime = (seconds) =>
+new Date(seconds * 1000).toISOString().substring(11,19);
+
+const handleLoadedMetadata = () =>{
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
+};
+
+const handleTimeupdate =() =>{
+    currenTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+};
+const handleTimelineChange = (event) => {
+    const { 
+        target: {value},
+    } = event;
+    video.currentTime =value;
+}
+
+const handlefullScreen = () =>{
+    const fullScreen = document.fullscreenElement;
+    if(fullScreen){
+        document.exitFullscreen();
+        fullScreen.innerText = "Enter Full Screen";
+    }else{
+        video.requestFullscreen();
+        fullScreen.innerText = "Exit Full Screen";
+
+    }
+};
+
+const hideControls =() =>  videoControls.classList.remove("showing");
+
+const handleMouseMove = () =>{
+    if(controlsTimeout){
+        clearTimeout(controlsTimeout);
+        controlsTimeout=null;
+    }
+    if(controlsMovementTimeout){
+        clearTimeout(controlsMovementTimeout);
+        controlsMovementTimeout = null;
+    }
+    videoControls.classList.add("showing");
+    //controlsMovementTimeout가 id일때
+    controlsMovementTimeout = setTimeout(hideControls,3000);
+};
+
+// 아래문장이 위의 한문장으로 대체가능!!
+const handleMouseLeave = () =>{
+    controlsTimeout = setTimeout(() => {
+        videoControls.classList.remove("showing");
+    },3000);
+    
+};
+
+playBtn.addEventListener("click",handlePlayClick);
+muteBtn.addEventListener("click",handleMute);
 volumeRange.addEventListener("input",handleVolumeChange);
+video.addEventListener("loadedmetadata",handleLoadedMetadata);
+video.addEventListener("timeupdate",handleTimeupdate);
+timeline.addEventListener("input",handleTimelineChange);
+fullScreen.addEventListener("click",handlefullScreen);
+video.addEventListener("mousemove",handleMouseMove);
+video.addEventListener("mouseleave",handleMouseLeave);
