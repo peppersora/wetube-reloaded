deb618dbf10a9018500649e1227447efe7213302
peppersora <110614387+peppersora@users.noreply.github.com>
2022-12-22 21:52:11 +0900

    video recording

--- client/js/recorder.js
+++ client/js/recorder.js
@@ -3,8 +3,41 @@
 const startBtn = document.getElementById("startBtn");
 const preview = document.getElementById("preview");
 
-const handleStart = async() => {
-    const stream = await navigator.mediaDevices.getUserMedia({
+//function은 외부에 있는 변수를 받을 수 있다.
+let stream;
+let recorder;
+
+const handleDownload = () => {};
+
+const handleStop = () =>{
+    startBtn.innerText = "Start Recording";
+    startBtn.addEventListener("click",handleStart);
+    startBtn.removeEventListener("click",handleStop);
+
+    recorder.stop();
+};
+
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click",handleStart);
+    startBtn.addEventListener("click",handleStop);
+    recorder = new MediaRecorder(stream);
+
+    recorder.ondataavailable = (event) =>{
+        
+        const videoFile = URL.createObjectURL(event.data);
+        preview.srcObject = null;
+        preview.src = videoFile;
+        preview.loop =true;
+        preview.play();
+    };
+    
+        recorder.start();
+       
+};
+
+const init = async() => {
+    stream = await navigator.mediaDevices.getUserMedia({
         audio:false,
         video:{width:200,height:100},
     });
@@ -12,5 +45,7 @@ const handleStart = async() => {
     // srcObject는 video가 가질수 있는 무언가
     preview.play();
 };
+
+init();
 
 startBtn.addEventListener("click",handleStart);
